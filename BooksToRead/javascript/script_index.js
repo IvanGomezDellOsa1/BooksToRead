3c1c59c68f9ab8a492c4a755b3b85988beee6b03
Ivan Gomez DellOsa <ivangomezdellosa@outlook.com>
2024-05-20 08:54:16 -0300

    Nav bar (mobile and desktop)

--- BooksToRead/javascript/script_index.js
+++ BooksToRead/javascript/script_index.js
@@ -1,5 +1,14 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
+
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.getElementById("nav");
+
+    hamburger.addEventListener('click', function () {
+        nav.classList.toggle("show");
+        hamburger.classList.toggle("show");
+    });
+
     let tabla = document.getElementById("tabla_libros");
 
     // Obtener datos del sessionStorage
--- BooksToRead/javascript/script_registrarse.js
+++ BooksToRead/javascript/script_registrarse.js
@@ -1,33 +1,42 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
+    
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.getElementById("nav");
+
+    hamburger.addEventListener('click', function () {
+        nav.classList.toggle("show");
+        hamburger.classList.toggle("show");
+    });
+
     let captcha_form = document.querySelector('.captcha_form');
     let captcha_desafio = document.querySelector('.captcha_desafio');
     let captcha_resultado = document.getElementById('captcha_resultado');
 
-let numero_desafio = Math.floor(100000 + Math.random() * 90000);   // Genera un numero aleatorio de 6 digitos entre 100000 y 999999
+    let numero_desafio = Math.floor(100000 + Math.random() * 90000);   // Genera un numero aleatorio de 6 digitos entre 100000 y 999999
 
-captcha_desafio.innerHTML = numero_desafio; //Imprime el numero aleatorio de 6 digitos
+    captcha_desafio.innerHTML = numero_desafio; //Imprime el numero aleatorio de 6 digitos
 
-function detenerSubmit(e){
-    e.preventDefault();
-    console.log('Envio submit detenido')
-    let captchaData = new FormData(captcha_form);
-    let numero_respuesta = Number(captchaData.get('numero_respuesta')); //Toma el dato que el usuario ingreso como respuesta al CAPTCHA (Transforma de String a numero)
-    evaluarRespuesta(numero_respuesta, numero_desafio);
-};
+    function detenerSubmit(e){
+        e.preventDefault();
+        console.log('Envio submit detenido')
+        let captchaData = new FormData(captcha_form);
+        let numero_respuesta = Number(captchaData.get('numero_respuesta')); //Toma el dato que el usuario ingreso como respuesta al CAPTCHA (Transforma de String a numero)
+        evaluarRespuesta(numero_respuesta, numero_desafio);
+    };
 
-function evaluarRespuesta(numero_respuesta, numero_desafio){
-    captcha_resultado.classList.remove('captcha_resultado_positivo', 'captcha_resultado_negativo'); //Evita que si hay mas de un intento se acumulen las clases
-    if (numero_respuesta === numero_desafio){
-        captcha_resultado.classList.add('captcha_resultado_positivo');
-        captcha_resultado.innerHTML = 'Resultado correcto(Envio completado)';
-    } else{
-        captcha_resultado.classList.add('captcha_resultado_negativo');
-        captcha_resultado.innerHTML = 'Resultado incorrecto(Intente de nuevo)';
+    function evaluarRespuesta(numero_respuesta, numero_desafio){
+        captcha_resultado.classList.remove('captcha_resultado_positivo', 'captcha_resultado_negativo'); //Evita que si hay mas de un intento se acumulen las clases
+        if (numero_respuesta === numero_desafio){
+            captcha_resultado.classList.add('captcha_resultado_positivo');
+            captcha_resultado.innerHTML = 'Resultado correcto(Envio completado)';
+        } else{
+            captcha_resultado.classList.add('captcha_resultado_negativo');
+            captcha_resultado.innerHTML = 'Resultado incorrecto(Intente de nuevo)';
+        }
     }
-}
 
-captcha_form.addEventListener('submit', detenerSubmit);
+    captcha_form.addEventListener('submit', detenerSubmit);
 
 });
 
