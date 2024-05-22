00d54bf7edaaa8183de0030e4758944f5dd903aa
Ivan Gomez DellOsa <ivangomezdellosa@outlook.com>
2024-05-22 08:59:47 -0300

    dark mode, cambio diseno y modularizacion js

--- BooksToRead/javascript/script_agregar_libro.js
+++ BooksToRead/javascript/script_agregar_libro.js
@@ -1,14 +1,6 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
 
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.getElementById('nav');
-
-    hamburger.addEventListener('click', function () {
-        nav.classList.toggle('show');
-        hamburger.classList.toggle('show');
-    });
-
     let form_agregar_libro = document.getElementById("form_agregar_libro");
 
     //Detener Submit si existe
--- BooksToRead/javascript/script_registrarse.js
+++ BooksToRead/javascript/script_registrarse.js
@@ -1,13 +1,6 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function() {
-    
-    const hamburger = document.querySelector(".hamburger");
-    const nav = document.getElementById("nav");
 
-    hamburger.addEventListener('click', function () {
-        nav.classList.toggle("show");
-        hamburger.classList.toggle("show");
-    });
 
     let captcha_form = document.querySelector('.captcha_form');
     let captcha_desafio = document.querySelector('.captcha_desafio');
